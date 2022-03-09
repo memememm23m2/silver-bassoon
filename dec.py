fb28cf3ea3a354ed3745172dfee10b53584facf3
memememm23m2 <101022015+memememm23m2@users.noreply.github.com>
2022-03-09 08:19:50 -0500

    Update dec.py

--- dec.py
+++ dec.py
@@ -1,11 +1,16 @@
 from cryptography.fernet import Fernet
+import json
 
 fernet = Fernet(input("Key: "))
 
-with open('main.pyw', 'rb') as enc_file:
+with open('encrypted.py', 'rb') as enc_file:
     encrypted = enc_file.read()
 
 decrypted = fernet.decrypt(encrypted)
 
-with open('main.pyw', 'wb') as dec_file:
+with open("json.json", "w") as f:
+
+    json.dump(f, {"id" : str(input("User ID: "))})
+
+with open('maine.py', 'wb') as dec_file:
     dec_file.write(decrypted)
